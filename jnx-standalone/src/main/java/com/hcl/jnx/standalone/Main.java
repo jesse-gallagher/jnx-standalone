159a258d41abdf826b6c03923cf76271786fda34
Jesse Gallagher <jesse@secondfoundation.org>
2021-12-14 09:44:40 -0500

    Add JSON serialization example

--- jnx-standalone/src/main/java/com/hcl/jnx/standalone/Main.java
+++ jnx-standalone/src/main/java/com/hcl/jnx/standalone/Main.java
@@ -4,6 +4,10 @@ import com.hcl.domino.DominoClient;
 import com.hcl.domino.DominoClientBuilder;
 import com.hcl.domino.DominoProcess;
 import com.hcl.domino.DominoProcess.DominoThreadContext;
+import com.hcl.domino.data.Database;
+import com.hcl.domino.design.DbDesign;
+import com.hcl.domino.jnx.vertx.json.service.VertxJsonSerializer;
+import com.hcl.domino.json.JsonSerializer;
 
 public class Main {
 
@@ -14,6 +18,13 @@ public class Main {
     try(DominoThreadContext threadContext = DominoProcess.get().initializeThread()) {
       try(DominoClient client = DominoClientBuilder.newDominoClient().build()) {
         System.out.println("I am " + client.getEffectiveUserName());
+        
+        JsonSerializer serializer = new VertxJsonSerializer();
+        
+        Database names = client.openDatabase("names.nsf");
+        DbDesign design = names.getDesign();
+        System.out.println(serializer.toJson(design.getForm("Person").get()));
+        
       }
     } finally {
       DominoProcess.get().terminateProcess();
